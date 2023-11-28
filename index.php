e1511d68f6f516d3d418b53348364724e4685818
jaber <jaberksh3012@gmail.com>
2023-11-28 12:02:08 +0100

    maquette proto

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/style.css">
+    <link rel="stylesheet" href="/public/css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Cocotons</title>
+</head>
+
+<body>
+    <header>
+        <img src="/img/logo-cocotons.png">
+        <a href="#"> Connexion/Deconnexion </a>
+    </header>
+
+    <div class="container">
+        <nav class="side-bar">
+            <div class="side-bar-elements">
+                <a href="#">Entrées</a>
+                <a href="#">Plats</a>
+                <a href="#">Desserts</a>
+                <a href="#">Petits déjeunés</a>
+                <a href="#">Goutés</a>
+            </div>
+
+        </nav>
+
+        <main>
+            <div class="search-bar">
+                <input type="text" class="search-input" placeholder="Que va-t'on manger aujourd'hui">
+            </div>
+            <p class="title">Nos recettes du moments</p>
+            <div class="separator"></div>
+            <div class="tri">
+                <p> trier par <a href="#"> plus recent </a> </p>
+            </div>
+            <div class="recettes-container">
+                <div class="recette-content">
+
+                    <img src="/img/recette-1.jpg">
+                    <p class="info">meru grillé</p>
+                    <div class="profil">
+                        <img src="/img/profil-cocotons.jpg" class="img-profil">
+                        <div class="label-profil">jaber benz</div>
+                    </div>
+                    <div class="rating">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="recette-content">
+
+                    <img src="/img/recette-1.jpg">
+                    <p class="info">meru grillé</p>
+                    <div class="profil">
+                        <img src="/img/profil-cocotons.jpg" class="img-profil">
+                        <div class="label-profil">jaber benz</div>
+                    </div>
+                    <div class="rating">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="recette-content">
+
+                    <img src="/img/recette-1.jpg">
+                    <p class="info">meru grillé</p>
+                    <div class="profil">
+                        <img src="/img/profil-cocotons.jpg" class="img-profil">
+                        <div class="label-profil">jaber benz</div>
+                    </div>
+                    <div class="rating">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="recette-content">
+
+                    <img src="/img/recette-1.jpg">
+                    <p class="info">meru grillé</p>
+                    <div class="profil">
+                        <img src="/img/profil-cocotons.jpg" class="img-profil">
+                        <div class="label-profil">jaber benz</div>
+                    </div>
+                    <div class="rating">
+                        <div class="stars">
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                            <i class="fa-solid fa-star"></i>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </main>
+
+    </div>
+    <script src="/public/js/app.js"></script>
+</body>
+
+</html>
